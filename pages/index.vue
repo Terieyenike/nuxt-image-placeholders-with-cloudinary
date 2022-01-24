13010ba14977c07e9c42231fd5b759b1c0d7126f
Teri Eyenike <teyenike1@gmail.com>
2022-01-24 13:54:44 +0100

    Fix: the home page for image placeholders

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="lg:flex items-center justify-between">
+  <div class="lg:flex items-center h-screen">
     <div class="w-full px-8">
-      <h1 class="md:text-3xl lg:text-5xl font-bold text-stone-800">IMAGE <span class="block">PLACEHOLDER</span></h1>
-      <h2 class="mt-4 text-blue-700">using Cloudinary</h2>
+      <h1 class="md:text-5xl lg:text-4xl font-bold text-stone-800">
+        IMAGE LOADING <span class="block">PLACEHOLDER</span>
+      </h1>
+      <h2 class="md:mb-4 lg:mt-3 text-blue-700">using Cloudinary and Nuxt</h2>
     </div>
 
-      <cld-image
-        :public-id="publicId"
-        loading="lazy"
-        gravity="auto:subject"
-        fetchFormat="auto"
-        quality="auto"
-        alt="An image example with Cloudinary"
-      >
-        <cld-placeholder type="pixelate" />
-      </cld-image>
+    <cld-image
+      :public-id="publicId"
+      loading="lazy"
+      gravity="auto:subject"
+      fetchFormat="auto"
+      quality="auto"
+      alt="Calm teri sitting"
+    >
+      <cld-placeholder type="pixelate" />
+    </cld-image>
   </div>
 </template>
 
@@ -23,7 +25,7 @@ export default {
   name: "IndexPage",
   data() {
     return {
-      publicId: "his_view_kfqrfb",
+      publicId: "image-placeholders/teri",
     };
   },
 };
